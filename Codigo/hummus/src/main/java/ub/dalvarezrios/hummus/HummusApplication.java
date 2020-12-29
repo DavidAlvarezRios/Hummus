6eaea5b16d9e52668c7a0c628aa47aa44df2226e
DavidAlvarezRios <dalvarezrios@gmail.com>
2020-12-29 21:08:13 +0100

    init commit

---
+++ Codigo/hummus/src/main/java/ub/dalvarezrios/hummus/HummusApplication.java
@@ -0,0 +1,13 @@
+package ub.dalvarezrios.hummus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HummusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HummusApplication.class, args);
+    }
+
+}
---
+++ Codigo/hummus/src/test/java/ub/dalvarezrios/hummus/HummusApplicationTests.java
@@ -0,0 +1,13 @@
+package ub.dalvarezrios.hummus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HummusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
